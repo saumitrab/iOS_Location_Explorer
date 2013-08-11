ea19111beeba11bb60dab40b1ef35546444f33c2
Saumitra <saumitra@yahoo-inc.com>
2013-08-11 14:28:20 -0700

    Initial Commit

---
+++ ExplorerTests/ExplorerTests.m
@@ -0,0 +1,36 @@
+//
+//  ExplorerTests.m
+//  ExplorerTests
+//
+//  Created by Saumitra Bhanage on 8/11/13.
+//  Copyright (c) 2013 yahoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ExplorerTests : XCTestCase
+
+@end
+
+@implementation ExplorerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
